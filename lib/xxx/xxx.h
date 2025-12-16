524eaeea4df2eab6f610d40359102bfd57e35dc7
Yaroslav Saprykin <yaroslav.saprykin@gmail.com>
2025-12-16 11:23:29 +0100

    code update

---
+++ lib/xxx/xxx.h
@@ -0,0 +1,17 @@
+#ifndef xxx_h
+#define xxx_h
+
+#include <Arduino.h>
+
+// WiFi network credentials
+extern const char* ssid;
+extern const char* password;
+
+// telegram bot credentials
+extern const char* botToken;
+extern const char* CHAT_ID;
+
+// actual timezone
+extern String actualTZ;
+
+#endif
